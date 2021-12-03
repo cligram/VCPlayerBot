3ef95c5b505da343c42687c9fd0cf3958b2c92cf
jim926241 <95039359+jim926241@users.noreply.github.com>
2021-12-03 19:58:15 +0330

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1784,9 +1784,9 @@ def convert(seconds):
 
 def get_pause(status):
     if status == True:
-        return "ادامه"
+        return "Resume"
     else:
-        return "مکث"
+        return "Pause"
 
 #https://github.com/pytgcalls/pytgcalls/blob/dev/pytgcalls/types/input_stream/video_tools.py#L27-L38
 def resize_ratio(w, h, factor):
