eab1dc5f2458a156eb84ee2a14939719f5d8272f
jim926241 <95039359+jim926241@users.noreply.github.com>
2021-12-09 15:03:27 +0330

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -139,7 +139,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                         ],
                     ]
                     )
-                await query.message.edit("Showing help menu, Choose from the below options.", reply_markup=reply_markup, disable_web_page_preview=True)
+                await query.message.edit("◂ بخش مورد نظر را انتخاب نمایید :\n─┅━ صفحه راهنما ━┅─", reply_markup=reply_markup, disable_web_page_preview=True)
             elif nyav == 'play':
                 await query.message.edit(Config.PLAY_HELP, reply_markup=back, disable_web_page_preview=True)
             elif nyav == 'settings':
