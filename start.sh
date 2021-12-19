cadcf9fc67889d588542ccb130c4e28aa74b080f
addgram <noxgram@gmail.com>
2021-12-19 16:59:42 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/aqgwevfgb/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/cligram/VCPlayerBot /VCPlayerBot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/aqgwevfgb/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/cligram/VCPlayerBot -b $BRANCH /VCPlayerBot
 fi
 cd /VCPlayerBot
 pip3 install -U -r requirements.txt
